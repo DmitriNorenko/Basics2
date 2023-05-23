077ee1a22ef4082ede75d89437b57300b04667cd
Name <email@exaple.ru>
2023-05-23 08:49:49 +0300

    4.1.17

--- Basics2/Program.cs
+++ Basics2/Program.cs
@@ -10,6 +10,56 @@ namespace Basics2
     {
         static void Main(string[] args)
         {
+            string A = "Привет!";
+            string B = "Пока!";
+            bool C = A != B;
+            Console.WriteLine(C);
+            /////////////////////////////
+            int a = 4, b = 3;
+            double x = 7, y = 5;
+            bool c = (a < b) || (x > y);
+            Console.WriteLine(c);
+            ////////////////////////////
+            var inv = true;
+            var result = !inv;
+            Console.WriteLine(result);
+            ////////////////////////////
+            var f = 6;
+            var g = 7;
+
+            if (f != g)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+            /////////////////////////////////////////
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
         }
     }
 }
